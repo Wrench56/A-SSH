20dff0a9b35f4a46086e872ea86536f290b96c49
Wrench56 <dmarkreg@gmail.com>
2024-07-08 01:36:18 -0400

    Add `fullscreen` page

---
+++ pages/fullscreen/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  
+</script>
+
+<p>Hello World</p>
+
+<style>
+  :root {
+    background-color: black;
+  }
+
+	p {
+    margin: 10px 0px 0px 0px;
+    font-family: monaco, Consolas, "Lucida Console", monospace;
+    color: whitesmoke;
+  }
+</style>
