66b4b65c311b209c7d18e64e1ad71230ca51484d
Wrench56 <dmarkreg@gmail.com>
2024-07-08 23:48:55 -0400

    Fix submodule imports in `main.py`

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,12 @@
 from plugins import base_plugin
-from plugins.plugins.A_SSH.backend import ssh, ui
+from plugins.plugins.A_SSH.backend.ui import fullscreen
+from plugins.plugins.A_SSH.backend.ssh import service
 
 
 class Plugin(base_plugin.Plugin):
     def load(self) -> bool:
-        ui.fullscreen.init()
-        ssh.service.init()
+        fullscreen.init()
+        service.init()
         return True
 
     def unload(self) -> bool:
