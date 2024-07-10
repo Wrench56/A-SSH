854e6bef787cab00a2ee2684d6e5f89f8b9f9eb4
Wrench56 <dmarkreg@gmail.com>
2024-07-10 02:03:44 -0400

    Remove page "header" from `fullscreen`

--- pages/fullscreen/+page.svelte
+++ pages/fullscreen/+page.svelte
@@ -2,17 +2,10 @@
   import SSHContainer from "$lib/plugins/A_SSH/SSHContainer.svelte";
 </script>
 
-<p>A-SSH</p>
 <SSHContainer />
 
 <style>
   :root {
     background-color: black;
-  }
-
-	p {
-    margin: 10px 0px 0px 0px;
-    font-family: monaco, Consolas, "Lucida Console", monospace;
-    color: whitesmoke;
   }
 </style>
