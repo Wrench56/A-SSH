e4afc68834782e23828f01d798bc4198d28db64b
Wrench56 <dmarkreg@gmail.com>
2024-07-12 02:09:11 -0400

    Set `localhost` as the default host in `login.py`

--- backend/ssh/login.py
+++ backend/ssh/login.py
@@ -8,14 +8,21 @@ async def parse(data: Dict[str, str]) -> Tuple[str, int, str, str]:
     if password is None:
         logging.error('Corruption detected in login data in A-SSH')
         return ('Invalid', 0, '', '')
-    user_loc = data.get('user_and_location')
-    if user_loc is None:
+    user_loc_unsplit = data.get('user_and_location')
+    if user_loc_unsplit is None:
         logging.error('Corruption detected in login data in A-SSH')
         return ('Invalid', 0, '', '')
-    user, loc = user_loc.split('@', 1)
-    if len(loc.split(':', 1)) > 1:
-        host, port_ = loc.split(':', 1)
-        port = int(port_)
+    user_loc_split = user_loc_unsplit.split('@', 1)
+    if len(user_loc_split) == 1:
+        user = user_loc_split[0]
+        host = 'localhost'
+        port = 22
     else:
-        host, port = loc, 22
+        user, loc = user_loc_split
+        if len(loc.split(':', 1)) > 1:
+            host, port_ = loc.split(':', 1)
+            port = int(port_)
+        else:
+            host, port = loc, 22
+
     return (host, port, user, password)
